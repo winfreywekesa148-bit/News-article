cebe059498e8c34bdeb16cef778080218096af2b
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-05-31 19:00:16 +0300

    Fix indentation in count_sentences function

--- sentences.py
+++ sentences.py
@@ -1,9 +1,8 @@
 #count number of sentences 
 def count_sentences(str):
-    # Split the string into sentences based on punctuation marks
+    
     sentences = str.split('.')
     
-    # Count the number of sentences
     num_sentences = len(sentences)
     
     return num_sentences
