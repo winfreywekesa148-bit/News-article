4541d6b7814d542c0d6e2f0d1d2208629e518bb5
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-01 15:23:40 +0300

    Delete paragraphs.py

--- paragraphs.py
+++
@@ -1,9 +0,0 @@
-# count number paragraphs 
-def count_paragraphs(str):
-    # Split the string into paragraphs based on newline characters
-    paragraphs = str.split('\n\n')
-    
-    # Count the number of paragraphs
-    num_paragraphs = len(paragraphs)
-    
-    return num_paragraphs
