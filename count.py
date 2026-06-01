a6a79b17cad942b9826872fac2f2f0401973b81a
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-01 14:25:25 +0300

    Refactor word counting function for user input

--- count.py
+++ count.py
@@ -1,11 +1,16 @@
 # counts the number of times a specified word appears in a news article
-def count_word_in_article(article, word):
-        
+def count_specific_word(str1, str2):
+    #Input
+    str1 = input("Enter the news article: ")
+    str2 = input("Enter the word to count: ")
+
     # Split the article into words
-    words = article.split()
+    words = str1.split()
     
     # Count the occurrences of the specified word
-    count = words.count(word)
+    count = words.count(str2)
 
     return count
-    
+
+print(count_specific_word("", "")) 
+
